e716b904c0a3b1969c849dd74db80451d19410ae
Diana Canela <DCanela80@gmail.com>
2023-12-01 10:03:07 -0700

    completed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <marquee><h1>React Learning is FUN!!!</h1></marquee>
+    <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*y6C4nSvy2Woe0m7bWEn4BA.png"></img>
+
+    </>
+  )
+}
+
+export default App
